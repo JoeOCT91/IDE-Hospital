f84df635a6c49d0c891dd03feb9fa973b49ac39e
Yousef Mohamed <unlimitsmaer@gmail.com>
2020-12-23 20:59:56 +0200

    Last commit

--- IDEHospital/Views/Custom Views/HospitalButton.swift
+++ IDEHospital/Views/Custom Views/HospitalButton.swift
@@ -14,14 +14,14 @@ class HospitalButton: UIButton {
         configure()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     init(frame: CGRect, tittle: String) {
         super.init(frame: frame)
         self.setTitle(tittle, for: .normal)
         configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     private func configure(){
