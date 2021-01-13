cc895a288286c466e35db5f94291e2e0dc555997
Yousef Mohamed <unlimitsmaer@gmail.com>
2021-01-13 22:49:32 +0200

    Some orgnization

--- IDEHospital/Extensions/UIViewController/SetupFunction+UINavigationController.swift
+++ IDEHospital/Extensions/UIViewController/SetupFunction+UINavigationController.swift
@@ -23,15 +23,3 @@ extension UINavigationController {
     }
     
 }
-
-extension UIViewController {
-    func hideKeyboardWhenTappedAround() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-    
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-}
